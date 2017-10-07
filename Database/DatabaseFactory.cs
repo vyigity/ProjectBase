207eb68afb01328fada334ffeb8e38cfa5bc99b1
vyigity <vyigity@hotmail.com>
2017-10-07 16:25:13 +0300

    Güncelleme

--- Database/DatabaseFactory.cs
+++ Database/DatabaseFactory.cs
@@ -46,7 +46,7 @@ namespace ProjectBase.Database
             }
             else if (conStr.ProviderName == "Oracle.ManagedDataAccess.Client")
             {
-                return new OracleManagedDatabase2(setting;
+                return new OracleManagedDatabase2(setting);
             }
             else if (conStr.ProviderName == "System.Data.SqlClient")
             {
