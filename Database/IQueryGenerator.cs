07243b2bac62cdd970d091c8a7ad9934411f9f3a
vyigity <vyigity@hotmail.com>
2017-12-02 20:56:39 +0300

    UPDATE
    Extended QueryGenerator's parameter functions.

--- Database/IQueryGenerator.cs
+++ Database/IQueryGenerator.cs
@@ -8,25 +8,63 @@ namespace ProjectBase.Database
     public interface IQueryGenerator
     {
         /// <summary>
+        /// Query generator will use this string as table name while generating update and insert statements.
+        /// </summary>
+        string TableName { get; set; }  
+        /// <summary>
+        /// Query generator will use this string as main sql query text. It can be used for any kind of command like DML and DDL. It can be used mainly for a select query.
+        /// </summary>
+        string SelectText { get; set; }
+        /// <summary>
         /// Query generator will concate this string to end of query. String must include sql key word like WHERE. For parameter usage in query, symbols of : or @ can be used. For UPDATE generation, this must be used for specify filter text.
         /// </summary>
         string FilterText { get; set; }
         /// <summary>
-        /// Query generator will use this string as procedure name. It can be a database function or procedure.
-        /// </summary>
-        string ProcedureName { get; set; }
-        /// <summary>
-        /// Query generator will concate this string to end of query. It can be used for group by expressions. For parameter usage in query, symbols of : or @ can be used.
+        /// Query generator will concate this string to end of query. It can be used for group by expressions.
         /// </summary>
         string SelectTail { get; set; }
         /// <summary>
-        /// Query generator will use this string as main sql query text. It can be used for any kind of command like DML and DDL. It can be used mainly for a select query. For parameter usage in query, symbols of : or @ can be used.
+        /// Query generator will use this string as procedure name. It can be a database function or procedure.
         /// </summary>
-        string SelectText { get; set; }
+        string ProcedureName { get; set; }
+
         /// <summary>
-        /// Query generator will use this string as table name while generating update and insert statements.
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
         /// </summary>
-        string TableName { get; set; }
+        void AddFilterParameter(string parameterName, object value);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object value, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object dbType, object value, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object value, int size, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object value, int size, byte scale, byte precision, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object dbType, object value, int size, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, object dbType, object value, int size, byte scale, byte precision, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, DbType dbType, object value, int size, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// </summary>
+        void AddFilterParameter(string parameterName, DbType dbType, object value, int size, byte scale, byte precision, ParameterDirection direction);
+
         /// <summary>
         /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
         /// </summary>
@@ -46,38 +84,47 @@ namespace ProjectBase.Database
         /// <summary>
         /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
         /// </summary>
+        void AddDataParameter(string parameterName, object value, int size, byte scale, byte precision, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
+        /// </summary>
         void AddDataParameter(string parameterName, object dbType, object value, int size, ParameterDirection direction);
+        /// <summary>
+        /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
+        /// </summary>
+        void AddDataParameter(string parameterName, object dbType, object value, int size, byte scale, byte precision, ParameterDirection direction);
         /// <summary>
         /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
         /// </summary>
         void AddDataParameter(string parameterName, DbType dbType, object value, int size, ParameterDirection direction);
         /// <summary>
-        /// Query generator will use this parameter for non-generated sql statement that is given with SelectText property.
+        /// Query generator will use this parameter while generating update and insert statements or procedure calls. For statement generation, parameter name must be same with column name in database table.
         /// </summary>
-        void AddFilterParameter(string parameterName, object value);
-        /// <summary>
-        /// Clears all query generater instance.
-        /// </summary>
-        void Clear();
-        /// <summary>
-        /// Returns generated insert command.
-        /// </summary>
-        IDbCommand GetInsertCommand();
+        void AddDataParameter(string parameterName, DbType dbType, object value, int size, byte scale, byte precision, ParameterDirection direction);     
+
         /// <summary>
         /// Returns a database returned parameter.
         /// </summary>
         object GetParameterValue(string parameterName);
         /// <summary>
-        /// Returns generated procedure command.
+        /// Returns generated insert command.
         /// </summary>
-        IDbCommand GetProcedure();
-        /// <summary>
-        /// Returns generated general command.
-        /// </summary>
-        IDbCommand GetSelectCommandBasic();
+        IDbCommand GetInsertCommand();
         /// <summary>
         /// Returns generated update command.
         /// </summary>
         IDbCommand GetUpdateCommand();
+        /// <summary>
+        /// Returns generated general command.
+        /// </summary>
+        IDbCommand GetSelectCommandBasic();       
+        /// <summary>
+        /// Returns generated procedure command.
+        /// </summary>
+        IDbCommand GetProcedure();
+        /// <summary>
+        /// Clears all query generater instance.
+        /// </summary>
+        void Clear();
     }
 }
